5c63a2909a2d7c57972825847d8e95202daca084
CairoTheo <arditheo98@gmail.com>
2022-07-21 19:55:25 +0700

    js result

--- index.js
+++ index.js
@@ -1,78 +1,77 @@
 let buttonTest = () => {
-  let testButton = document.getElementsByClassName('container')
-  testButton[0].style.display = 'none'
+  let testButton = document.getElementsByClassName("container");
+  testButton[0].style.display = "none";
 
-  let testButton2 = document.getElementsByClassName("test")
-  testButton2[0].style.display = 'block'
+  let testButton2 = document.getElementsByClassName("test");
+  testButton2[0].style.display = "block";
 
   let inputData = {
-    nama: '',
-    gender: ''
-  }
-  
-  let inputNama = document.getElementById("floatingTextarea").value
-  let inputGender = document.getElementById("gender-user").value
-  
-  inputData.nama = inputNama
-  inputData.gender = inputGender
+    nama: "",
+    gender: "",
+  };
 
-  console.log(inputData)
-}
+  let inputNama = document.getElementById("floatingTextarea").value;
+  let inputGender = document.getElementById("gender-user").value;
+
+  inputData.nama = inputNama;
+  inputData.gender = inputGender;
+
+  console.log(inputData);
+};
 
 // Color Blind Test
 function colorBlindTest() {
-
   let test = [
     {
-      question: '1.png',
-      answer: '1'
+      question: "1.png",
+      answer: "1",
     },
     {
-      question: '2.png',
-      answer: '2'
+      question: "2.png",
+      answer: "2",
     },
     {
-      question: '3.png',
-      answer: '3'
+      question: "3.png",
+      answer: "3",
     },
     {
-      question: '4.png',
-      answer: '4'
+      question: "4.png",
+      answer: "4",
     },
     {
-      question: '5.png',
-      answer: ''
+      question: "5.png",
+      answer: "5",
     },
     {
-      question: '6.png',
-      answer: ''
+      question: "6.png",
+      answer: "7",
     },
     {
-      question: '7.png',
-      answer: ''
+      question: "7.png",
+      answer: "7",
     },
     {
-      question: '8.png',
-      answer: ''
+      question: "8.png",
+      answer: "8",
     },
     {
-      question: '9.png',
-      answer: ''
+      question: "9.png",
+      answer: "1",
     },
     {
-      question: '10.png',
-      answer: ''
+      question: "10.png",
+      answer: "1",
     },
-  ]
+  ];
 
-  random =  Math.floor(Math.random() * test.length);
+  random = Math.floor(Math.random() * test.length);
 
-  BlindTest = test[random]
-  
+  BlindTest = test[random];
+
   //let questionElement = document.getElementById('question')
   //questionElement.innerHTML = colorBlindTest.question
 
-  return BlindTest
+  return BlindTest;
 }
 
 console.log(colorBlindTest());
@@ -80,6 +79,27 @@ console.log(colorBlindTest());
 //
 let answerButton = (num) => {
   // let theAnswer = document.getElementsByClassName("btn-1")
-  console.log(num)
-  return num
+  console.log(num);
+  return num;
+};
+
+// let inputResult = {
+//   user: inputData.nama,
+// };
+
+let obj = {
+  user: "person",
+};
+
+let { user } = obj;
+console.log(user);
+
+let totalTrue = 10;
+let totalFalse = 0;
+let jawaban = "";
+
+if (totalTrue === 10) {
+  jawaban = `Hi ${user}, Score ${totalTrue * 10} Mata Kamu Normal!`;
+} else {
+  jawaban = `Hi ${user}, Score${totalTrue * 10} Kamu Terindikasi Buta Warna`;
 }
